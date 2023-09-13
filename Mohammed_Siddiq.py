add403c86c6b6c2a568e0e17c11e83b301e49fda
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2023-09-13 20:45:13 +0530

    Changing file name

---
+++ Mohammed_Siddiq.py
@@ -0,0 +1,38 @@
+#importing opencv
+import cv2
+
+# Create a CascadeClassifier object and load the face detection model
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Open the default camera (camera index 0)
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, video_data = video_capture.read()
+
+    # Convert the frame to grayscale for face detection
+    col = cv2.cvtColor(video_data, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(
+        col,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw rectangles around detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(video_data, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Display the frame with detected faces
+    cv2.imshow("video_live", video_data)
+
+    # Check for the 'a' key to exit the loop
+    if cv2.waitKey(10) == ord("a"):
+        break
+
+# Release the video capture object and close the OpenCV window
+video_cap.release()
+cv2.destroyAllWindows()
